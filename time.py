0043148bd08d0723aac6f428a881754aef42b7d5
Akira_0809 <akira_0809@icloud.com>
2023-05-07 00:32:49 +0900

    time.pyの消去

--- time.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-from django.core.management import call_command
-
-while True:
-    now = time.localtime()
-    if now.tm_hour == 3 and now.tm_min == 0 and now.tm_sec == 0:
-        call_command("update_db")
-    time.sleep(1)
